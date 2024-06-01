056a4f61c67669929a0e17409bb12454486e38a2
Liubov <drogaltseva92@gmail.com>
2024-06-01 21:18:37 +0200

    add sett

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    resolve: {
+        alias: {
+            src: '/src',
+            components: '/src/components',
+            pages: '/src/pages',
+            assets: '/src/assets',
+            api: '/src/api',
+            helpers: '/src/helpers',
+        },
+    },
+    base: '/camper',
+});
