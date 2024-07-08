9433733dbad93d9bd0f38f8c2676b26b5881cfea
Liubov <drogaltseva92@gmail.com>
2024-07-08 14:45:59 +0200

    fix route

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistor} loading={null}>
-        <BrowserRouter basename="/camper_front">
+        <BrowserRouter basename="/">
           <App />
         </BrowserRouter>
       </PersistGate>
